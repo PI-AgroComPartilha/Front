a2be4afdbb3373d5d592596fb66853eca2645dc0
Vitor Oliveira. <caulicons@gmail.com>
2024-06-07 12:08:34 -0300

    updating loading feature

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -5,8 +5,8 @@ type LoadingProps = {
 function Loading({ title }: LoadingProps) {
   return (
     <div className="flex flex-col items-center justify-center min-h-[90vh] gap-4 select-none">
-      <img src="/carrot.svg" alt="" className="w-20 animate-bounce" />
-      <p className="text-3xl font-semibold">{title}</p>
+      <img src="/carrot.svg" alt="" className="w-24 animate-bounce" />
+      <p className="text-4xl font-semibold">{title}</p>
     </div>
   );
 }
